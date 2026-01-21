32aa74d8582c8923b840730a443f5b00cff07a2b
JOSE_ESPAÑA <jespman300@g.educaand.es>
2026-01-21 19:51:04 +0100

    JOSE_ESPAÑA: Actualización del archivo examen.py

--- examen.py
+++ examen.py
@@ -2,5 +2,5 @@
 def iniciar_sistema():
     print("Iniciando sistema de evaluación...")
     print("Versión 1.0 - Conexión establecida.")
-
+    print("Jose Francisco España Mañas")
 iniciar_sistema()
